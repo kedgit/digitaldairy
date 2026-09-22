456ebd28f2a0c0aae1fd8a643bfc3dcf25dcc3eb
kedar <kedarmhetre0410@gmail.com>
2026-09-22 16:23:29 +0530

    Update Docker configuration

--- version1/src/main/java/com/milkdairy/version1/Version1Application.java
+++ version1/src/main/java/com/milkdairy/version1/Version1Application.java
@@ -5,13 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+
 @SpringBootApplication
 @EnableScheduling
 @EnableCaching
 public class Version1Application {
-
 	public static void main(String[] args) {
+
 		SpringApplication.run(Version1Application.class, args);
+	
 	}
 
 }
